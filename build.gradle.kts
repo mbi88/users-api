0b1ba31bd01038eaca8fd4971e3d18d122ff6008
Maksym Binkovskyi <mbinkovskiy@gmail.com>
2023-03-22 12:49:42 +0200

    build.gradle -> kts

---
+++ build.gradle.kts
@@ -0,0 +1,25 @@
+plugins {
+    java
+    id("org.springframework.boot") version "3.0.4"
+    id("io.spring.dependency-management") version "1.1.0"
+}
+
+tasks.bootJar {
+    archiveBaseName.set("users-api")
+    archiveVersion.set("1.0")
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.springframework.boot:spring-boot-starter-validation")
+    implementation("org.postgresql:postgresql")
+    implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.0.4")
+    implementation("org.modelmapper:modelmapper:3.1.1")
+    implementation("io.hypersistence:hypersistence-utils-hibernate-60:3.2.0")
+}
